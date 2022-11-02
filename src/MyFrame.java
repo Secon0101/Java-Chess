20dd49af2ee47263a4e237e636065e940a24a553
나에유 <pressanykey567@gmail.com>
2022-11-02 09:28:26 +0900

    배열 정리

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,10 +1,34 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
 
 public class MyFrame extends JFrame{
     MyFrame()
     {
         setTitle("첫번째 프레임");
-        setSize(300,400);
+        setSize(500,500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JPanel jp = new JPanel();
+        jp.setLayout(new GridLayout(8,8));
+        jp.setBackground(Color.white);
+        int row = 72;
+
+        for(int i = 8; i > 0; i --)
+        {
+            int col = 1;
+            for(int j = 0; j < 8; j++)
+            {
+                jp.add(new JButton((char)row + Integer.toString(col)));
+                col++;
+            }
+            row--;
+        }
+
+
+        add(jp);
         setVisible(true);
     }
 }
