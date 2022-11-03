f98b5f91e1fd20c5be1c75c4c0dc4b6906e51d8c
나에유 <pressanykey567@gmail.com>
2022-11-03 14:54:07 +0900

    마우스 호버 완성

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import chess.*;
 public class Main {
     public static void main(String[] args) {
         Chess chess = new Chess();
-        
-        new MyFrame();
+
+        MyFrame frame = new MyFrame();
+
     }
 }
