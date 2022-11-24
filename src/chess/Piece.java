c1b31012128f549bb9c539c704525e8eeda4a647
Secon0101 <duwnsgur11012@gmail.com>
2022-11-24 09:11:26 +0900

    add getPosition()

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -1,13 +1,12 @@
 package chess;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public abstract class Piece {
     protected final Chess chess;
     protected final Team team;
     protected Position position;
-    protected List<Position> moves = new LinkedList<>();
+    protected LinkedList<Position> moves = new LinkedList<>();
     
     
     Piece(Chess chess, Team team, Position position) {
@@ -20,8 +19,11 @@ public abstract class Piece {
     public Team getTeam() {
         return team;
     }
-    public List<Position> getMoves() {
+    public LinkedList<Position> getMoves() {
         return moves;
+    }
+    public Position getPosition() {
+        return position;
     }
     void setPosition(Position pos) {
         position = pos;
@@ -29,7 +31,7 @@ public abstract class Piece {
     
     
     /** 현재 말이 이동 가능한 위치 리스트를 새로 계산한다. */
-    void calculateMoves() {
+     void calculateMoves() {
         moves.clear();
     }
 }
