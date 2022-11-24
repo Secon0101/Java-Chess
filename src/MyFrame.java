17c51f37ed86b5f288fad559e3782d924838d5a9
나에유 <pressanykey567@gmail.com>
2022-11-24 10:30:28 +0900

    폰이 대각선 못먹음 폰 처음에 2칸 못감 나이트 이동 불가능 호버 상태에서 색 바뀌는거 하면 이상하게 나옴 <= 고치려는 중임

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -11,13 +11,12 @@ public class MyFrame extends JFrame implements MouseListener {
     ImageIcon[] pieces = new ImageIcon[12];
     Color backCol;
     Chess myChess;
-    Piece selectedPiece;
+    Position startPos;
+    Position endPos;
+    JPanel mainPanel;
 
-    JLabel[][] displays = new JLabel[8][8];
-
-    MyFrame(Chess chess) {
-
-        myChess = chess;
+    public void SetIcons()
+    {
         pieces[0] = new ImageIcon("res/images/PB.png");
         pieces[1] = new ImageIcon("res/images/RB.png");
         pieces[2] = new ImageIcon("res/images/NB.png");
@@ -30,7 +29,12 @@ public class MyFrame extends JFrame implements MouseListener {
         pieces[9] = new ImageIcon("res/images/BW.png");
         pieces[10] = new ImageIcon("res/images/QW.png");
         pieces[11] = new ImageIcon("res/images/KW.png");
+    }
 
+    MyFrame(Chess chess) {
+
+        myChess = chess;
+        SetIcons();
         for(int i = 0; i < 12; i++)
         {
             Image img = pieces[i].getImage();
@@ -43,30 +47,34 @@ public class MyFrame extends JFrame implements MouseListener {
         setResizable(false);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        UpdateFrame();
+    }
 
-        JPanel jp = new JPanel();
-        jp.setLayout(new GridLayout(8, 8));
-        jp.setBackground(Color.darkGray);
+    public void UpdateFrame()
+    {
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(8, 8));
+        mainPanel.setBackground(Color.darkGray);
 
         for (int i = 8; i > 0; i--) {
             for (int j = 1; j <= 8; j++) {
                 Color blankCol = new Color(0, 0, 0);
-                PieceBlank njp = new PieceBlank();
+                PieceBlank blank = new PieceBlank();
 
                 if ((j + (i%2)) % 2 == 0)
                     blankCol = new Color(255, 255, 255);
                 else
                     blankCol = new Color(100, 100, 100);
 
-                njp.setBackground(blankCol);
+                blank.setBackground(blankCol);
                 JLabel jl = new JLabel();
-                njp.posX = j;
-                njp.posY = i;
+                blank.posX = j;
+                blank.posY = i;
                 jl.setSize(60,60);
 
                 if(myChess.getPiece(j,i) != null)
                 {
-                    Piece curPiece = chess.getPiece(j,i);
+                    Piece curPiece = myChess.getPiece(j,i);
                     int plusIndex = 0;
                     if(curPiece.getTeam() == Team.WHITE)
                     {
@@ -98,32 +106,31 @@ public class MyFrame extends JFrame implements MouseListener {
                         jl.setIcon(pieces[plusIndex + 5]);
                     }
                 }
-
-
-                njp.add(jl);
-                jp.add(njp);
-                njp.addMouseListener(this);
+                blank.add(jl);
+                mainPanel.add(blank);
+                blank.addMouseListener(this);
             }
         }
-        add(jp);
+        add(mainPanel);
         setVisible(true);
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
         PieceBlank pieceBlank = (PieceBlank)e.getSource();
-        selectedPiece = myChess.getPiece(pieceBlank.posX, pieceBlank.posY);
+        startPos = new Position(pieceBlank.posX, pieceBlank.posY);
         System.out.println("From. " + pieceBlank.posX + " / " + pieceBlank.posY);
     }
     @Override
     public void mouseReleased(MouseEvent e) {
-        if(selectedPiece != null)
+        if(startPos != null && endPos != null)
         {
-            PieceBlank pieceBlank = (PieceBlank)e.getSource();
-            Position endPos = new Position(pieceBlank.posX, pieceBlank.posY);
+            PieceBlank pieceFull = (PieceBlank)e.getSource();
             System.out.println("To. " + endPos.x + " / " + endPos.y);
-            System.out.println(myChess.move(selectedPiece.getPosition(), endPos));
-            selectedPiece = null;
+            System.out.println(myChess.move(startPos, endPos));
+            startPos = null;
+            endPos = null;
+            UpdateFrame();
         }
     }
     @Override
@@ -132,14 +139,21 @@ public class MyFrame extends JFrame implements MouseListener {
     }
     @Override
     public void mouseEntered(MouseEvent e) {
-        JPanel b = (JPanel)e.getSource();
+        PieceBlank b = (PieceBlank)e.getSource();
         backCol = b.getBackground();
-        b.setBackground(new Color(153,255,102));
+        //b.setBackground(new Color(153,255,102));
+        if(startPos != null)
+        {
+            endPos = new Position(b.posX, b.posY);
+        }
     }
     @Override
     public void mouseExited(MouseEvent e) {
-        JPanel b = (JPanel)e.getSource();
-        b.setBackground(backCol);
+        PieceBlank b = (PieceBlank)e.getSource();
+        if ((b.posX + (b.posY%2)) % 2 == 0)
+            b.setBackground(new Color(255, 255, 255));
+        else
+            b.setBackground(new Color(100, 100, 100));
     }
 }
 
